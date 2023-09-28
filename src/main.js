130128282d9311e277dc9e6a8d432483e51cae1d
haikali3 <105930061+haikali3@users.noreply.github.com>
2023-09-28 20:16:55 +0800

    tailwindsetup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import './assets/main.css'
+import './assets/tailwind.css';
+import router from './router';
+import { createApp } from 'vue';
+import App from './App.vue';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+createApp(App).mount('#app');
 
-createApp(App).mount('#app')
+app.use(router);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./index.html', './src/**/*. {vue, js, ts, jsx, tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "weather-primary": "#fff200"
+        "weather-secondary": "#807900"
+      }
+    },
+    fontFamily:{
+      Roboto: ["Roboto,sans-serif"],
+    },
+    container:{
+      padding: "2rem",
+      center: true,
+    },
+    screens:{
+      sm: "640px",
+      md: "768px",
+    },
   },
   plugins: [],
-}
-
+};
